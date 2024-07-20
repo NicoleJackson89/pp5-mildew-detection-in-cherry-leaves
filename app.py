b5c481b2a10ed53d4523924606d74b1e8445b31e
Nicole Jackson <nicolejackson8990@gmail.com>
2024-07-20 13:12:02 +0000

    Add: Page Visualizer to app pages

--- app.py
+++ app.py
@@ -3,10 +3,12 @@ from app_pages.multipage import MultiPage
 
 # loads the pages scripts
 from app_pages.page_summary import page_summary_body
+from app_pages.page_cherry_leaves_visualizer import page_cherry_leaves_visualizer_body
 
-app = MultiPage(app_name="Powdery Mildew Detection - Cherry Leaves")  # Creates an instance of the app
+app = MultiPage(app_name="Mildew Detection - Cherry Leaves")  # Creates an instance of the app
 
 # app pages are added here using .add_page()
 app.add_page("Quick Project Summary", page_summary_body)
+app.add_page("Cherry Leaves Visualiser", page_cherry_leaves_visualizer_body)
 
 app.run()  # Run the app
