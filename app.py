563f052c2349634543d46ac57415bacba2d662b3
Nicole Jackson <nicolejackson8990@gmail.com>
2024-07-21 13:59:21 +0000

    Add: Page ml performance to app pages

--- app.py
+++ app.py
@@ -6,6 +6,7 @@ from app_pages.page_summary import page_summary_body
 from app_pages.page_cherry_leaves_visualizer import page_cherry_leaves_visualizer_body
 from app_pages.page_powdery_mildew_detection import page_powdery_mildew_detection_body
 from app_pages.page_project_hypothesis import page_project_hypothesis_body
+from app_pages.page_ml_performance import page_ml_performance_metrics
 
 app = MultiPage(app_name="Mildew Detection - Cherry Leaves")  # Creates an instance of the app
 
@@ -14,5 +15,6 @@ app.add_page("Quick Project Summary", page_summary_body)
 app.add_page("Cherry Leaves Visualiser", page_cherry_leaves_visualizer_body)
 app.add_page("Powdery Mildew detection", page_powdery_mildew_detection_body)
 app.add_page("Project Hypothesis", page_project_hypothesis_body)
+app.add_page("ML Performance Metrics", page_ml_performance_metrics)
 
 app.run()  # Run the app
---
+++ app_pages/page_ml_performance.py
@@ -0,0 +1,48 @@
+import streamlit as st
+import matplotlib.pyplot as plt
+import pandas as pd
+from matplotlib.image import imread
+from src.machine_learning.evaluate_clf import load_test_evaluation
+
+
+def page_ml_performance_metrics():
+    version = 'v2'
+
+    st.write("### Label Frequencies on Train, Validation and Test Sets")
+
+    st.info(
+        f" The cherry leaves dataset was divided into three subsets:\n\n"
+        f" * The training set comprises 1,472 images, representing 70% of the entire dataset. This data is used to "
+        f" train the model, enabling it to generalize and make predictions on new, unseen data.\n\n"
+        f" * The validation set comprises 210 images, representing 10% of the entire dataset. Assists in enhancing the "
+        f" model's performance by refining it after each epoch, which is a full pass of the training set through the model.\n\n"
+        f" * The test set comprises 422 images, representing 20% of the entire dataset. Provides information about the model's "
+        f" final accuracy after the training phase is completed. This evaluation uses a batch of data that the model has never seen before.")
+
+    labels_distribution = plt.imread(f"outputs/{version}/label_dist.png")
+    st.image(labels_distribution, caption='Labels Distribution on Train, Validation and Test Sets')
+    st.write("---")
+
+
+    st.write("### Model History")
+
+    st.info(
+        f" Accuracy measures how closely the model's predictions (accuracy) match the true data (val_acc).\n"
+        f" A good model that performs well on unseen data demonstrates its ability to generalize and avoid overfitting to the training dataset.\n\n"
+        f" The loss is the total of errors made for each example in the training (loss) or validation (val_loss) sets.\n"
+        f" The loss value indicates how poorly or well a model performs after each optimization iteration.")
+
+    col1, col2 = st.beta_columns(2)
+    with col1: 
+        model_acc = plt.imread(f"outputs/{version}/model_training_accuracy.png")
+        st.image(model_acc, caption='Model Training Accuracy')
+    with col2:
+        model_loss = plt.imread(f"outputs/{version}/model_training_losses.png")
+        st.image(model_loss, caption='Model Training Losses')
+    st.write("---")
+
+    st.write("### Generalised Performance on Test Set")
+
+    st.dataframe(pd.DataFrame(load_test_evaluation(version), index=['Loss', 'Accuracy']))
+    
+    
